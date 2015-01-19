273debf0c979145744a3dafa7c5b4fe32da613fe
JR <jrcasey@gmail.com>
2015-01-19 13:59:51 -0500

    test

--- src/org/usfirst/frc/team78/robot/Robot.java
+++ src/org/usfirst/frc/team78/robot/Robot.java
@@ -16,6 +16,9 @@ import org.usfirst.frc.team78.robot.subsystems.ExampleSubsystem;
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
+ * 
+ * We are now in Github!
+ * 
  */
 public class Robot extends IterativeRobot {
 
